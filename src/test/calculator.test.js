6c9a1de519272def50ee17451ea3d69381268a1c
ansmeer008 <ansmeer008@gmail.com>
2023-03-10 15:00:33 +0900

    calculator 테스트 코드 : beforeEach 적용해 재작성

--- src/test/calculator.test.js
+++ src/test/calculator.test.js
@@ -1,47 +1,68 @@
 const Calculator = require("../calculator");
 
-test("Calculator의 초기값은 0입니다.", () => {
-  const cal = new Calculator();
-  expect(cal.value).toBe(0);
-});
+//describe를 이용하면 여러 테스트 코드를 묶어서 작성할 수 있다.
+//it은 Caltulator 테스트를 가리치는 삼인칭이라고 생각하면 됨.
 
-test("set 매서드를 이용하면 value 값을 설정할 수 있습니다.", () => {
-  const cal = new Calculator();
-  cal.set(3);
-  expect(cal.value).toBe(3);
-});
+//const cal = new Calculator 가 반복되므로 it 바깥으로 꺼내서 사용하고자 할 수 있지만
+//각 테스트들은 독립적으로 동작하는 것이 중요하므로 이전에 호출한 object가 다음에 영향을 미치지 않게 해야 함.
+//이럴 때 사용할 수 있는 게 beforeEach()와 afterEach()이다.
 
-test("clear 매서드를 이용하면 value 값을 0으로 초기화합니다.", () => {
-  const cal = new Calculator();
-  cal.set(5);
-  cal.clear();
-  expect(cal.value).toBe(0);
-});
+//+@ beforeAll, afterAll과 같이 모든 테스트가 시작되기 전이나 후에 행동을 설정해줄 수 있는 함수도 존재한다!
 
-test("add 매서드를 이용하면 value 값에 입력 값을 더하기 연산 합니다.", () => {
-  const cal = new Calculator();
-  cal.set(3);
-  cal.add(4);
-  expect(cal.value).toBe(7);
-});
+describe("Calculator", () => {
+  let cal;
+  beforeEach(() => {
+    cal = new Calculator();
+  });
 
-test("substract 매서드를 이용하면 value 값에 입력 값을 빼기 연산 합니다.", () => {
-  const cal = new Calculator();
-  cal.set(7);
-  cal.substract(2);
-  expect(cal.value).toBe(5);
-});
+  it("inits with 0", () => {
+    expect(cal.value).toBe(0);
+  });
 
-test("multiply 매서드를 이용하면 value 값에 입력 값을 곱하기 연산 합니다.", () => {
-  const cal = new Calculator();
-  cal.set(2);
-  cal.multiply(5);
-  expect(cal.value).toBe(10);
-});
+  it("sets", () => {
+    cal.set(9);
+    expect(cal.value).toBe(9);
+  });
 
-test("divide 매서드를 이용하면 value 값을 입력 값으로 나누기 연산 합니다.", () => {
-  const cal = new Calculator();
-  cal.set(16);
-  cal.divide(2);
-  expect(cal.value).toBe(8);
+  it("clear", () => {
+    cal.set(9);
+    cal.clear();
+    expect(cal.value).toBe(0);
+  });
+
+  it("adds", () => {
+    cal.set(1);
+    cal.add(2);
+    expect(cal.value).toBe(3);
+  });
+
+  it("substracts", () => {
+    cal.substract(1);
+    expect(cal.value).toBe(-1);
+  });
+
+  it("multiplies", () => {
+    cal.set(9);
+    cal.multiply(4);
+    expect(cal.value).toBe(36);
+  });
+
+  describe("divides", () => {
+    it("0/0 === NaN", () => {
+      cal.divide(0);
+      expect(cal.value).toBe(NaN);
+    });
+
+    it("1 / 0 === Infinity", () => {
+      cal.set(1);
+      cal.divide(0);
+      expect(cal.value).toBe(Infinity);
+    });
+
+    it("4 / 4 === 1", () => {
+      cal.set(4);
+      cal.divide(4);
+      expect(cal.value).toBe(1);
+    });
+  });
 });
