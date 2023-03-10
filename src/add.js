2d7a61ed191bb77439804ca69da8ee8b5f8ecbbf
ansmeer008 <ansmeer008@gmail.com>
2023-03-10 14:14:14 +0900

    Jest watch 옵션 설정 실습

--- src/add.js
+++ src/add.js
@@ -3,3 +3,8 @@ function add(a, b) {
 }
 
 module.exports = add;
+
+//테스트에 --watch 옵션을 사용하기 위해서는 git을 함께 사용한다.
+//git에 커밋되지 않았지만 작성된 파일들이 존재할 때 해당 테스트를 watch하고,
+//만약 모든 파일들이 commit 되어 있으면 npm test 했을 때
+//"No tests found related to files changed since last commit"과 같은 문구가 뜸
