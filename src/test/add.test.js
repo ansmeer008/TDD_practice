753f05e0dc4359ae2c6bac71c11107b924ed7da2
ansmeer008 <ansmeer008@gmail.com>
2023-03-10 14:09:57 +0900

    Jest setup & add

---
+++ src/test/add.test.js
@@ -0,0 +1,6 @@
+const add = require("../add");
+
+//test("테스트 이름", 어떤 테스트를 수행할 건지 콜백함수로 전달, 비동기 함수에 한해 얼마만큼의 시간을 허용할 건지 timeout에 대한 정보도 전달 가능)
+test("adds 1 + 2 to equal 3", () => {
+  expect(add(1, 2)).toBe(3);
+});
